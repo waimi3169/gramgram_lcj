7774bfe6a07996d8ec83a306197c9b8b38b7a778
깃waimi3169 <waimi3169@naver.com>
2023-04-27 13:16:13 +0900

    세팅

---
+++ src/main/java/com/ll/gramgram3/Gramgram3Application.java
@@ -0,0 +1,13 @@
+package com.ll.gramgram3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gramgram3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gramgram3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/gramgram3/Gramgram3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.gramgram3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gramgram3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
