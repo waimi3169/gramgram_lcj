770ad07f8101e5bea054c417b5d07f57769847f8
깃waimi3169 <waimi3169@naver.com>
2023-05-01 16:20:37 +0900

    로그인 내비바 추가 , 각 페이지 진입전 로그인체크 구현

--- src/main/java/com/ll/gramgram3/boundedContext/member/service/MemberService.java
+++ src/main/java/com/ll/gramgram3/boundedContext/member/service/MemberService.java
@@ -11,10 +11,12 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true) // 아래 메서드들이 전부 readOnly라는 것을 명시
+@Transactional(readOnly = true) // 아래 메서드들이 전부 readonly 라는 것을 명시, 나중을 위해
 public class MemberService {
     private final PasswordEncoder passwordEncoder;
-    private MemberRepository memberRepository;
+
+    private final MemberRepository memberRepository;
+
     public Optional<Member> findByUsername(String username) {
         return memberRepository.findByUsername(username);
     }
