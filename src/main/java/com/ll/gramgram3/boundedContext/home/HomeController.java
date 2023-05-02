d89621b8f4dac341ad4cdd7c7c35f667c4cca465
깃waimi3169 <waimi3169@naver.com>
2023-05-02 13:43:04 +0900

    내 정보 페이지를 rq를 통하여 구현

--- src/main/java/com/ll/gramgram3/boundedContext/home/HomeController.java
+++ src/main/java/com/ll/gramgram3/boundedContext/home/HomeController.java
@@ -1,11 +1,15 @@
 package com.ll.gramgram3.boundedContext.home;
 
+import com.ll.gramgram3.boundedContext.member.entity.Member;
+import com.ll.gramgram3.boundedContext.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.security.Principal;
 import java.util.Enumeration;
 
 @Controller
@@ -28,6 +32,6 @@ public class HomeController {
             sb.append(String.format("%s: %s\n", attributeName, attributeValue));
         }
 
-        return sb.toString();
+        return sb.toString().replaceAll("\n", "<br>");
     }
 }
--- src/main/java/com/ll/gramgram3/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/gramgram3/boundedContext/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.gramgram3.boundedContext.member.controller;
 
+import com.ll.gramgram3.boundedContext.member.entity.Member;
 import com.ll.gramgram3.boundedContext.member.service.MemberService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -9,9 +10,12 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/member")
